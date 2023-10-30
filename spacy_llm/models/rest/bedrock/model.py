945d61f6f36778a3c19227246feae521f6d88410
Vivek Silimkhan <vivek_silimkhan@persistent.com>
2023-10-31 01:06:26 +0530

    Added Titan and Jurassic models

--- spacy_llm/models/rest/bedrock/model.py
+++ spacy_llm/models/rest/bedrock/model.py
@@ -2,27 +2,56 @@ import json
 import os
 import warnings
 from enum import Enum
-from typing import Any, Dict, Iterable, List, Optional
+from typing import Any, Dict, Iterable, List, Optional, Tuple
 
+
+from ..base import REST
 
 class Models(str, Enum):
     # Completion models
     TITAN_EXPRESS = "amazon.titan-text-express-v1"
+    TITAN_LITE = "amazon.titan-text-lite-v1"
+    AI21_JURASSIC_ULTRA = "ai21.j2-ultra-v1"
+    AI21_JURASSIC_MID = "ai21.j2-mid-v1"
 
+class ModelParams(List, Enum):
+    # Params with default values
+    TITAN = ["maxTokenCount", "stopSequences", "temperature", "topP"]
+    AI21_JURASSIC = ["maxTokens", "temperature", "topP", "countPenalty", "presencePenalty", "frequencyPenalty"]
 
-class Bedrock:
+class Bedrock(REST):
     def __init__(
-        self, model_id: str, region: str, config: Dict[Any, Any], max_retries: int = 5
+        self, model_id: str, region: str, config: Dict[Any, Any], max_tries: int = 5,
     ):
         self._region = region
         self._model_id = model_id
-        self._config = config
-        self._max_retries = max_retries
+        self._max_tries = max_tries
+        self.strict = True
+        self.endpoint = f"https://bedrock-runtime.{self._region}.amazonaws.com"
+        self._config = {}
 
-    def get_session_kwargs(self) -> Dict[str, Optional[str]]:
+        if self._model_id in [Models.TITAN_EXPRESS, Models.TITAN_LITE]:
+            config_params = ModelParams.TITAN
+        if self._model_id in [Models.AI21_JURASSIC_ULTRA, Models.AI21_JURASSIC_MID]:
+            config_params = ModelParams.AI21_JURASSIC
+
+        for i in config_params:
+            self._config[i] = config[i]
+
+        super().__init__(
+            name=model_id,
+            config=self._config,
+            max_tries=max_tries,
+            strict = True,
+            endpoint = "",
+            interval = 3,
+            max_request_time = 30
+        )
+
+    def get_session_kwargs(self) -> Dict[str,str]:
 
         # Fetch and check the credentials
-        profile = os.getenv("AWS_PROFILE") if not None else ""
+        profile = os.getenv("AWS_PROFILE") if not None else "default"
         secret_key_id = os.getenv("AWS_ACCESS_KEY_ID")
         secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
         session_token = os.getenv("AWS_SESSION_TOKEN")
@@ -51,9 +80,9 @@ class Bedrock:
                 "set up by making it available as an environment variable AWS_SESSION_TOKEN."
             )
 
-        assert secret_key_id is not None
-        assert secret_access_key is not None
-        assert session_token is not None
+        # assert secret_key_id is not None
+        # assert secret_access_key is not None
+        # assert session_token is not None
 
         session_kwargs = {
             "profile_name": profile,
@@ -80,7 +109,7 @@ class Bedrock:
 
             session_kwargs = self.get_session_kwargs()
             session = boto3.Session(**session_kwargs)
-            api_config = Config(retries=dict(max_attempts=self._max_retries))
+            api_config = Config(retries=dict(max_attempts=self._max_tries))
             bedrock = session.client(service_name="bedrock-runtime", config=api_config)
             accept = "application/json"
             contentType = "application/json"
@@ -90,19 +119,47 @@ class Bedrock:
                 accept=accept,
                 contentType=contentType,
             )
-            responses = json.loads(r["body"].read().decode())["results"][0][
-                "outputText"
-            ]
+            if self._model_id in [Models.TITAN_EXPRESS, Models.TITAN_LITE]:
+                responses = json.loads(r["body"].read().decode())["results"][0]["outputText"]
+            elif self._model_id in [Models.AI21_JURASSIC_ULTRA, Models.AI21_JURASSIC_MID]:
+                responses = json.loads(r["body"].read().decode())['completions'][0]['data']['text']
+            
             return responses
 
         for prompt in prompts:
-            if self._model_id in [Models.TITAN_LITE, Models.TITAN_EXPRESS]:
+            if self._model_id in [Models.TITAN_EXPRESS, Models.TITAN_LITE]:
                 responses = _request(
                     json.dumps(
                         {"inputText": prompt, "textGenerationConfig": self._config}
                     )
                 )
+            if self._model_id in [Models.AI21_JURASSIC_ULTRA, Models.AI21_JURASSIC_MID]:
+                responses = _request(
+                    json.dumps(
+                        {"prompt":prompt, **self._config}
+                    )
+                ) 
 
             api_responses.append(responses)
 
         return api_responses
+
+    def _verify_auth(self) -> None:
+        try:
+            import boto3
+            from botocore.config import Config
+            from botocore.exceptions import NoCredentialsError
+            from botocore.exceptions import ClientError
+
+            session_kwargs = self.get_session_kwargs()
+            session = boto3.Session(**session_kwargs)
+            bedrock = session.client(service_name="bedrock")
+            models = bedrock.list_foundation_models()
+        except NoCredentialsError:
+            raise NoCredentialsError
+
+    def credentials(self) -> Dict[str,str]:
+        return self.get_session_kwargs()
+
+    def get_model_names(self) -> Tuple[str, ...]:
+        return ("amazon.titan-text-express-v1", "amazon.titan-text-lite-v1", "ai21.j2-ultra-v1", "ai21.j2-mid-v1")
